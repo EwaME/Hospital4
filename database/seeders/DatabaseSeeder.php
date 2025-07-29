ef1c5f497700e440879815401181346727966644
EwaME <edwardmaradiagaespinal@gmail.com>
2025-07-28 22:47:04 -0600

    Avance commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         $roleAdmin = Role::create(['name' => 'Admin']);
         $rolePaciente = Role::create(['name' => 'Paciente']);
         $roleDoctor = Role::create(['name' => 'Doctor']);
